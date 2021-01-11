3319b5657174326e05b706df277794a3fd59a0f4
程璐 <695625950@qq.com>
2021-01-11 14:09:52 +0800

    Spring引入测试

--- Source/Server/src/com/youcha/Test.java
+++ Source/Server/src/com/youcha/Test.java
@@ -6,6 +6,8 @@ import com.youcha.dao.userDao.UserMapper;
 import com.youcha.entity.User;
 import com.youcha.util.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @Description 测试类
@@ -16,13 +18,16 @@ import org.apache.ibatis.session.SqlSession;
 public class Test {
 
 	public static void main(String[] args) {
-		SqlSession session = MyBatisUtil.getSession();
 
+		/*SqlSession session = MyBatisUtil.getSession();
 		UserMapper userMapper = session.getMapper(UserMapper.class);
 		List<User> users = userMapper.selectAllUsers();
 		for(User u : users) {
 		    System.out.println(u);
 		}
-		session.close();
+		session.close();*/
+		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+		User user = (User)ac.getBean("User");
+		System.out.println(user);
 	}
 }
