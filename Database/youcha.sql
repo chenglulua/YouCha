346a0f63af49502bde7a1c13712fe4f855f5bffb
程璐 <695625950@qq.com>
2021-02-17 19:09:30 +0800

    数据库更新

--- Database/youcha.sql
+++ Database/youcha.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80011
 File Encoding         : 65001
 
-Date: 2021-01-19 16:30:14
+Date: 2021-02-17 19:08:35
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,17 +20,17 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `assessment`;
 CREATE TABLE `assessment` (
-  `assId` int(11) NOT NULL,
+  `assId` int(11) NOT NULL AUTO_INCREMENT,
   `drinkId` int(11) DEFAULT NULL,
   `star` int(11) DEFAULT NULL,
   PRIMARY KEY (`assId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of assessment
 -- ----------------------------
 INSERT INTO `assessment` VALUES ('1', '1', '5');
-INSERT INTO `assessment` VALUES ('2', '2', '4');
+INSERT INTO `assessment` VALUES ('2', '1', '4');
 INSERT INTO `assessment` VALUES ('3', '30', '3');
 INSERT INTO `assessment` VALUES ('4', '17', '5');
 
@@ -56,11 +56,11 @@ CREATE TABLE `drink` (
 -- ----------------------------
 -- Records of drink
 -- ----------------------------
-INSERT INTO `drink` VALUES ('1', '经典奶茶', '1', '牛奶搭配红茶', '4', 'drinkImg/1.jpg', '1', '1', '1', '1', '5');
-INSERT INTO `drink` VALUES ('2', '经典奶绿', '1', '牛奶搭配绿茶', '4', 'drinkImg/2.jpg', '1', '1', '1', '1', '4');
-INSERT INTO `drink` VALUES ('3', '双拼奶茶', '1', '布丁与珍珠', '5', 'drinkImg/3.jpg', '1', '1', '0', '1', '4');
-INSERT INTO `drink` VALUES ('4', '奶茶三兄弟', '1', '布丁、珍珠与椰果', '6', 'drinkImg/4.jpg', '1', '1', '0', '1', '4');
-INSERT INTO `drink` VALUES ('5', '奶茶全家福', '1', '饱腹感极强', '7', 'drinkImg/5.jpg', '1', '1', '0', '0', '2');
+INSERT INTO `drink` VALUES ('1', '经典奶茶', '1', '牛奶搭配红茶', '4', 'drinkImg/1.jpg', '1', '1', '1', '1', '3');
+INSERT INTO `drink` VALUES ('2', '经典奶绿', '1', '牛奶搭配绿茶', '4', 'drinkImg/2.jpg', '1', '1', '1', '1', '3');
+INSERT INTO `drink` VALUES ('3', '奶茶双拼', '1', '布丁红豆', '4', 'drinkImg/3.jpg', '1', '1', '1', '1', '3');
+INSERT INTO `drink` VALUES ('4', '奶茶三兄弟', '1', '椰果布丁与红豆', '4', 'drinkImg/4.jpg', '1', '1', '1', '1', '3');
+INSERT INTO `drink` VALUES ('5', '奶茶全家福', '1', '饱腹感极强', '4', 'drinkImg/5.jpg', '1', '1', '1', '1', '3');
 INSERT INTO `drink` VALUES ('6', '鲜牛奶', '2', '新鲜牛奶', '4', 'drinkImg/6.jpg', '1', '1', '0', '1', '3');
 INSERT INTO `drink` VALUES ('7', '芒果牛奶', '2', '芒果搭配牛奶', '5', 'drinkImg/7.jpg', '1', '1', '0', '1', '3');
 INSERT INTO `drink` VALUES ('8', '草莓牛奶', '2', '草莓搭配牛奶', '5', 'drinkImg/8.jpg', '1', '1', '0', '1', '5');
@@ -80,7 +80,7 @@ INSERT INTO `drink` VALUES ('21', '青柠养乐多', '4', '青柠搭配养乐多
 INSERT INTO `drink` VALUES ('22', '红茶奶盖', '5', '咸香奶盖搭配奶茶', '6', 'drinkImg/22.jpg', '1', '1', '1', '1', '4');
 INSERT INTO `drink` VALUES ('23', '绿茶奶盖', '5', '咸香奶盖搭配奶绿', '6', 'drinkImg/23.jpg', '1', '1', '1', '1', '5');
 INSERT INTO `drink` VALUES ('24', '芒果奶盖', '5', '咸香奶盖与芒果果肉', '7', 'drinkImg/24.jpg', '1', '1', '0', '1', '5');
-INSERT INTO `drink` VALUES ('25', '黑糖珍珠奶盖茶', '5', '咸香奶盖与黑糖珍珠', '7', 'drinkImg/25.jpg', '1', '1', '0', '1', '4');
+INSERT INTO `drink` VALUES ('25', '经典奶盖', '5', '珍珠奶盖', '6', 'drinkImg/25.jpg', '1', '1', '0', '1', '3');
 INSERT INTO `drink` VALUES ('26', '奶盖牛乳茶', '5', '咸香奶盖搭配牛乳茶', '5', 'drinkImg/26.jpg', '0', '0', '0', '1', '5');
 INSERT INTO `drink` VALUES ('27', '抹茶芝士奶盖', '5', '芝士奶盖搭配抹茶', '7', 'drinkImg/27.jpg', '1', '1', '0', '1', '5');
 INSERT INTO `drink` VALUES ('28', '草莓冰激凌', '6', '草莓与冰激凌', '5', 'drinkImg/28.jpg', '0', '0', '0', '1', '5');
@@ -120,7 +120,7 @@ CREATE TABLE `ordertable` (
   `code` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `status` smallint(6) DEFAULT NULL,
   PRIMARY KEY (`orderId`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of ordertable
@@ -130,8 +130,8 @@ INSERT INTO `ordertable` VALUES ('2', '1', '12846059', '2020-12-6-15:05:03', '2'
 INSERT INTO `ordertable` VALUES ('3', '2', '18469875', '2021-1-2-11:28:26', '30', '3', null, null, null, 'large', '1', '5', '02001', '1');
 INSERT INTO `ordertable` VALUES ('4', '3', '12589456', '2021-1-2-11:30:10', '17', '4', null, null, null, 'large', '1', '7', '02002', '1');
 INSERT INTO `ordertable` VALUES ('5', '4', '12846059', '2021-1-3-18:16:23', '25', null, 'half', 'hot', '', 'middle', '1', '7', '02003', '0');
-INSERT INTO `ordertable` VALUES ('6', '5', '18469875', '2021-1-3-18:16:25', '22', null, 'half', 'normal', 'pearl', 'large', '2', '18', '02004', '0');
-INSERT INTO `ordertable` VALUES ('7', '5', '18469875', '2021-1-3-18:16:23', '7', null, 'none', 'ice', null, 'middle', '1', '5', '02004', '0');
+INSERT INTO `ordertable` VALUES ('6', '5', '18469875', '2021-1-3-18:16:25', '22', null, 'half', 'normal', 'pearl', 'large', '2', '18', '05004', '0');
+INSERT INTO `ordertable` VALUES ('7', '5', '18469875', '2021-1-3-18:16:23', '7', null, 'none', 'ice', null, 'middle', '1', '5', '05004', '0');
 
 -- ----------------------------
 -- Table structure for type
@@ -169,6 +169,6 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('12589456', '测试用户3', '13012149543', '123456');
-INSERT INTO `user` VALUES ('12846059', '测试用户1', '13301211646', '123456');
-INSERT INTO `user` VALUES ('18469875', '测试用户2', '12458465795', '123456');
+INSERT INTO `user` VALUES ('12589456', '测试用户3', '13012149543', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `user` VALUES ('12846059', '测试用户1', '13301211646', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `user` VALUES ('18469875', '测试用户2', '12458465795', 'e10adc3949ba59abbe56e057f20f883e');
